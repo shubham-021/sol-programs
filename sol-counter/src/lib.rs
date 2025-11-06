005677cc287675c715e4df26aee4089b3df5abfd
shubham-021 <shubham.arka@gmail.com>
2025-11-07 01:11:17 +0530

    sol/init

---
+++ sol-counter/src/lib.rs
@@ -0,0 +1,47 @@
+use borsh::{BorshSerialize,BorshDeserialize};
+use solana_program::{
+    account_info::{AccountInfo, next_account_info},
+    entrypoint::ProgramResult,
+    msg,pubkey::Pubkey
+};
+
+#[derive(BorshDeserialize,BorshSerialize)]
+enum InstructionType{
+    Increment(u32),
+    Decrement(u32)
+}
+
+
+#[derive(BorshDeserialize,BorshSerialize)]
+struct  Counter{
+    count: u32
+}
+
+solana_program::entrypoint!(counter_contract);
+
+pub fn counter_contract(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult {
+    
+    let acc = next_account_info(&mut accounts.iter())?;
+
+    let instruction_type = InstructionType::try_from_slice(instruction_data)?;
+    let mut counter_data = Counter::try_from_slice(&acc.data.borrow())?;
+    match instruction_type {
+        InstructionType::Increment(value) => {
+            msg!("Executing increment");
+            counter_data.count += value;
+        },
+        InstructionType::Decrement(value) => {
+            msg!("Executing decrement");
+            counter_data.count -= value;
+        }
+    }
+
+    counter_data.serialize(&mut *acc.data.borrow_mut());
+    msg!("Contract succeeded");
+
+    Ok(())
+}
